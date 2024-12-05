f428728726585529158e7b2d221692fa83f1f2af
Elias Bosire Ayunga <bosire.elias@gmail.com>
2024-12-05 05:43:07 +0300

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+let drivers = ['Elias', 'Bosire', 'Ayunga', 'Nina', 'Ebo']
+
+// 1. returnFirstTwoDrivers()
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+
+
+  //2.returnLastTwoDrivers 
+   
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+
+
+  //3. selectingDrivers
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+  console.log(selectingDrivers[0](['Elias', 'Bosire', 'Ayunga', 'Nina', 'Ebo']));
+
+  //4. fare multiplier
+
+  const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  };
+
+  // 5. fare doubler
+  const fareDoubler = createFareMultiplier(2);
+
+  //6. fare tripler
+
+  const fareTripler = createFareMultiplier(3);
+
+//7 driver selector
+const selectDifferentDrivers = function(drivers, driverFunction) {
+    return driverFunction(drivers);
+  };
